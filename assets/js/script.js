b36dc6f4cb0782e2fc0400c93cb807dca768e155
Jake Schaefbauer <jschaefmn@gmail.com>
2022-05-30 19:47:26 -0500

    finished 5.3.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,6 +166,55 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+
+// making the lists sortable
+$('.card .list-group').sortable({
+  connectWith: $('.card .list-group'),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activiate: function (event) {
+    console.log("activate", this);
+  },
+  deactiviate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    // array to store the task data in
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find('p')
+        .text()
+        .trim();
+      var date = $(this)
+        .find('span')
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr('id')
+      .replace("list-", "");
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
 // load tasks for the first time
 loadTasks();
 
